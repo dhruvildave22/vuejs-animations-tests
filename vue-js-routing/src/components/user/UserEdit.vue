1370dfd17839a8fcd58e9dfc522113200c7afc87
Dhruvil Dave <dhruvil@gkmit.co>
2020-06-10 18:38:06 +0530

    vue-js-routing module done

---
+++ vue-js-routing/src/components/user/UserEdit.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>Edit the User</h3>
+    <p>User Edited with ID: {{$route.params.id}}</p>
+
+    <button class="btn btn-primary" @click="confirmed = true">Confirm</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      confirmed: false
+    };
+  },
+  beforeRouteLeave(to, from, next) {
+    if (this.confirmed) {
+      next();
+    } else {
+      if (confirm("Are you Sure?")) {
+        next();
+      } else {
+        next(false);
+      }
+    }
+  }
+};
+</script>
