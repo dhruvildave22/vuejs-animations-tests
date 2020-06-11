2ba0e810cffeb7db9189f14a339519ce551de73f
Dhruvil Dave <dhruvil@gkmit.co>
2020-06-11 18:51:37 +0530

    stock market app added

---
+++ vue-js-stock-trader-example-app/src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Trade or View your Portfolio</h1>
+    <h6>You may save & load you data</h6>
+    <h6>Click on End Day to begin a new Day!</h6>
+    <hr />
+    <p>Your Funds: ${{ formatPrice(funds) }}</p>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    }
+  },
+  methods: {
+    formatPrice(value) {
+      let val = (value / 1).toFixed(2).replace(",", ".");
+      return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+  }
+};
+</script>
